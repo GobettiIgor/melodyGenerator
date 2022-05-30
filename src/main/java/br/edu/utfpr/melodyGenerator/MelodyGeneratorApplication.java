1b07f368d5e93fe1d7f8ce6f943988bb8bbcec64
GobettiIgor <igorgob@hotmail.com>
2022-05-30 10:37:09 -0300

    Initial commit

---
+++ src/main/java/br/edu/utfpr/melodyGenerator/MelodyGeneratorApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.melodyGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelodyGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelodyGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/utfpr/melodyGenerator/MelodyGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.melodyGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelodyGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
